fcda83eb027466951d3d2ab1d7db0b21d3f97b93
Gleb <409075@niuitmo.ru>
2026-02-06 15:19:16 +0300

    develop starting sidebar layout

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,14 +1,43 @@
 import { NavLink } from "react-router-dom";
+import "./Sidebar.css";
 
 function Sidebar() {
   return (
     <aside>
-      <div className="logoDiv"></div>
+      <div className="logoDiv">
+        <p>logo</p>
+        <img src="https://placehold.co/50" alt="" className="logoImg" />
+      </div>
       <nav>
-        <NavLink to="/">Main</NavLink>
-        <NavLink to="/settings">Settings</NavLink>
-        <NavLink to="/notifications">Notifications</NavLink>
+        <NavLink
+          to="/"
+          className={({ isActive }) =>
+            isActive ? "nav-link active" : "nav-link"
+          }
+        >
+          Main
+        </NavLink>
+        <NavLink
+          to="/settings"
+          className={({ isActive }) =>
+            isActive ? "nav-link active" : "nav-link"
+          }
+        >
+          Settings
+        </NavLink>
+        <NavLink
+          to="/notifications"
+          className={({ isActive }) =>
+            isActive ? "nav-link active" : "nav-link"
+          }
+        >
+          Notifications
+        </NavLink>
       </nav>
+      <div className="accountDiv">
+        <img src="src/assets/my_photo.jpg" alt="" className="userPhoto" />
+        <img src="https://placehold.co/400" alt="" className="exitLogo" />
+      </div>
     </aside>
   );
 }
