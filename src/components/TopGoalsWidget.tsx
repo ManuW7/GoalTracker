b91233a9d0c680a28dc0af7cd6f1210d4c3af1be
Gleb <409075@niuitmo.ru>
2026-02-16 13:49:49 +0300

    add topGoals static structure

--- src/components/TopGoalsWidget.tsx
+++ src/components/TopGoalsWidget.tsx
@@ -1,7 +1,25 @@
 import "./topGoalsWidget.css";
 
 function TopGoalsWidget() {
-  return <div className="topGoalsWidgetDiv"></div>;
+  return (
+    <div className="topGoalsWidgetDiv">
+      <h3>Top goals</h3>
+      <div className="goalsContainer">
+        <div className="goal">
+          <p className="goalName">Учеба</p>
+          <p className="goalPercent">50%</p>
+        </div>
+        <div className="goal">
+          <p className="goalName">Гитара</p>
+          <p className="goalPercent">25%</p>
+        </div>
+        <div className="goal">
+          <p className="goalName">Баскетбол</p>
+          <p className="goalPercent">25%</p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default TopGoalsWidget;
