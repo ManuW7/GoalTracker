b237a0f92fadacc989197d2ad2a8ce5a64f962e6
Gleb <409075@niuitmo.ru>
2026-02-19 16:54:54 +0300

    finish static new actions layout

--- src/components/AddActions.tsx
+++ src/components/AddActions.tsx
@@ -3,11 +3,14 @@ import "./addActions.css";
 function AddActions() {
   return (
     <div className="addActionDiv">
-      <h3>Add new actions</h3>
       <form action="">
+        <div className="headerWithDate">
+          <h3>Add new actions</h3>
+          <input type="date" name="actionDate" required />
+        </div>
         <div className="choseGoalDiv">
           <label htmlFor="goalsOptions">What goal are you persuing?</label>
-          <select name="goalsOptions" id="goalsOptions">
+          <select name="goalsOptions" id="goalsOptions" required>
             <option value="1">Option 1</option>
             <option value="2">Option 2</option>
           </select>
@@ -16,7 +19,11 @@ function AddActions() {
           <label htmlFor="actionDescription">
             Describe your action towards that goal
           </label>
-          <textarea name="actionDescription" id="actionDescription"></textarea>
+          <textarea
+            name="actionDescription"
+            id="actionDescription"
+            required
+          ></textarea>
         </div>
         <input type="submit" value={"Submit"} />
       </form>
