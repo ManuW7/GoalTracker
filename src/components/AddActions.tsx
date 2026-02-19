646b219ce1e9d945cd072fa3715e3143125fcd75
Gleb <409075@niuitmo.ru>
2026-02-19 16:37:09 +0300

    create static layout for creating actions div

--- src/components/AddActions.tsx
+++ src/components/AddActions.tsx
@@ -4,6 +4,22 @@ function AddActions() {
   return (
     <div className="addActionDiv">
       <h3>Add new actions</h3>
+      <form action="">
+        <div className="choseGoalDiv">
+          <label htmlFor="goalsOptions">What goal are you persuing?</label>
+          <select name="goalsOptions" id="goalsOptions">
+            <option value="1">Option 1</option>
+            <option value="2">Option 2</option>
+          </select>
+        </div>
+        <div className="actionDescriptionContainer">
+          <label htmlFor="actionDescription">
+            Describe your action towards that goal
+          </label>
+          <textarea name="actionDescription" id="actionDescription"></textarea>
+        </div>
+        <input type="submit" value={"Submit"} />
+      </form>
     </div>
   );
 }
