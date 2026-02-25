bd24999afac9d2e6c7e2d51625da890e0a95a9ec
Utkin Pavel <utkin.pv@phystech.edu>
2026-02-25 23:47:23 +0300

    Finish minim func of goals

--- backend/app/routers/goals.py
+++ backend/app/routers/goals.py
@@ -11,8 +11,28 @@ def get_goals():
     return goals
 
 @router.get("/{id}", response_model=Goal)
-def get_goal_id(id : int):
+def get_goal(id : int):
     goal = goal_editor.get_goal_id(id)
     if not goal:
-        raise HTTPException(status_code=404, detail="Goal not found")
+        raise HTTPException(status_code=404, detail="Goal not found for get")
     return goal
+
+@router.put("/{id}", response_model=Goal)
+def put_goal(id : int, goal : Goal):
+    edit = goal_editor.update_goal(id, goal)
+    if not edit: 
+        raise HTTPException(status_code=404, detail="Goal not found for put")
+    return edit
+
+@router.delete("/{id}")
+def delete_goal(id : int):
+    result = goal_editor.delete_goal(id)
+    if not result:
+        raise HTTPException(status_code=404, detail="Goal not found for delete")
+    return
+
+@router.post("/")
+def post_goal(goal : Goal):
+    if goal.id != -1:
+        raise HTTPException(status_code=404, detail="Post goal id not equal -1")
+    goal_editor.create_goal(goal)
--- backend/app/services/service.py
+++ backend/app/services/service.py
@@ -10,10 +10,13 @@ class GoalService:
             name="a",
             description="ab",
             color="#FFFFFF",
-            date_set="2026-02-02",
+            date_set="2026-01-15T10:00:00Z",
             user_id=100
         )
         _fake_db[1] = first_goal
+
+    def _get_uniq_id(self):
+        return max(_fake_db.keys()) + 1
 
     def get_goal_id(self, id : int) -> Goal | None:
         if id not in _fake_db: return None
@@ -21,3 +24,18 @@ class GoalService:
     
     def get_goals(self) -> list[Goal]:
         return list(_fake_db.values())
+    
+    def update_goal(self, id : int, goal : Goal) -> Goal | None:
+        if id not in _fake_db: return None
+        _fake_db[id] = goal
+        return _fake_db[id]
+    
+    def delete_goal(self, id : int) -> bool:
+        if id not in _fake_db: return False
+        del _fake_db[id]
+        return True
+    
+    def create_goal(self, goal : Goal):
+        new_id = self._get_uniq_id()
+        goal.id = new_id
+        _fake_db[new_id] = goal
