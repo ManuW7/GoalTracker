8353e38384b5522450e2e79fcdf7c0ada0446310
Utkin Pavel <utkin.pv@phystech.edu>
2026-02-26 19:36:00 +0300

    Add CORS for localhost

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,21 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from app.routers import goals, actions
 
 app = FastAPI()
 app.include_router(goals.router)
 app.include_router(actions.router)
+
+origins = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,          # какие origin разрешены
+    allow_credentials=True,         # разрешать cookies / auth
+    allow_methods=["*"],            # разрешить все методы (GET, POST и т.д.)
+    allow_headers=["*"],            # разрешить все заголовки
+)
