cda4affc7bd4b2d23a10576ea9e4a34ba2b4c4cc
Gleb <409075@niuitmo.ru>
2026-03-17 17:24:58 +0300

    add modal for adding actions

---
+++ Frontend/src/components/ModalAddActions.tsx
@@ -0,0 +1,11 @@
+import "./ModalAddAcrions.css";
+
+function ModalAddActions() {
+  return (
+    <div className="modalOutline">
+      <div className="modalContent">hi</div>
+    </div>
+  );
+}
+
+export default ModalAddActions;
