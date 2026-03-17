73790efe01963a01827c673a5649badab12d70ca
Gleb <409075@niuitmo.ru>
2026-03-17 16:09:44 +0300

    add auth

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,16 +1,26 @@
 import "./App.css";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Settings from "./components/Settings";
 import Dashboard from "./components/Dashboard";
 import Notifications from "./components/Notifications";
 import Layout from "./components/Layout";
 import Calendar from "./components/Calendar";
+import Auth from "./components/Auth";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Layout></Layout>}>
+        <Route path="/auth" element={<Auth></Auth>}></Route>
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <Layout></Layout>
+            </ProtectedRoute>
+          }
+        >
           <Route index element={<Dashboard></Dashboard>}></Route>
           <Route path="settings" element={<Settings></Settings>}></Route>
           <Route
@@ -19,6 +29,7 @@ function App() {
           ></Route>
           <Route path="calendar" element={<Calendar></Calendar>}></Route>
         </Route>
+        <Route path="*" element={<Navigate to="/" replace />}></Route>
       </Routes>
     </>
   );
